cb247dc8dafda779db6bac78c57f0c1527ecfcd3
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2018-03-04 14:50:51 -0500

    :sparkles: Add husky

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // Side effects imports
-import "libs/dotenv";
-import "libs/postgres";
+import 'libs/dotenv';
+import 'libs/postgres';
 
 // import * as Express from "express"; import * as cors from "cors";
